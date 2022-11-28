d53740ec317336a0b3268f59e7b64e4b0d59f80c
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2022-11-28 13:19:36 +0800

    add Transactional

--- src/main/java/org/john/crow/service/impl/UserServiceImpl.java
+++ src/main/java/org/john/crow/service/impl/UserServiceImpl.java
@@ -8,6 +8,8 @@ import org.john.crow.mapper.UserJpaRepository;
 import org.john.crow.pojo.entity.Users;
 import org.john.crow.service.UserService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -28,6 +30,7 @@ public class UserServiceImpl implements UserService {
         return userJpaRepository.findById(id);
     }
 
+    @Transactional(propagation = Propagation.SUPPORTS, rollbackFor = Exception.class)
     @Override
     public Optional<Users> queryByUserName(String username) {
         return userJpaRepository.findByUsername(username);
